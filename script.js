bd9893102a8fe9ffb31066bf617256e254771a26
Eraze86 <therese.rosvall@hotmail.se>
2021-11-24 12:06:40 +0100

    fixed issue with bot getting right number

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ const button = document.getElementById("button");
 
 button.addEventListener("click", function(){
 
+
 // koppla input. få något att hända bid de olika åldarna med if. 
 
 const number = document.getElementById("number")
-if(number <= 18)  {
-   console.log("du är för ung")
-} else if (number >= 65){
-    console.log("du är för gammal")
+if(number.value < 18)  {
+   alert("du är för ung")
+} else if (number.value > 65){
+    alert("du är för gammal")
 } else {
-    console.log("du är i rätt ålder")
+    alert("du är i rätt ålder")
 }
 });
 //varför ger den ut för gammal fast man skriver in mindre än 18?
